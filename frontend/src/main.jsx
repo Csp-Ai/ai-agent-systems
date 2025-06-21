ebc3324140d21c1ed6c7105feb36ab38861106be
Csp-Ai <poncechris95@gmail.com>
2025-06-20 17:18:25 -0700

    Add interactive use case selector page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import LandingPage from './LandingPage.jsx'
+import AgentsGallery from '../AgentsGallery.jsx'
+import UseCaseSelector from './UseCaseSelector.jsx'
 import DevToolsPanel from './DevToolsPanel.jsx'
+
+const path = window.location.pathname;
+
+let page = <LandingPage />;
+if (path.startsWith('/gallery')) page = <AgentsGallery />;
+if (path.startsWith('/use-cases')) page = <UseCaseSelector />;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <LandingPage />
+      {page}
       <DevToolsPanel />
     </>
   </StrictMode>
